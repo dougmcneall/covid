55a227892fee1e777c5eb451d32b32580abe51fb
dougmcneall <dougmcneall@gmail.com>
2020-05-13 12:02:38 +0100

    pre run SA design generator

--- GenerateSensDesign.R
+++ GenerateSensDesign.R
@@ -1,21 +1,27 @@
 # GenerateSensDesign.R
+# Generate a design for a simple sensitivity analysis of MetaWards.
 
 # Choose some limits for incubation period, rzero, infectious period etc.
 
+# From a MetaWards error message:
 # Put the result through Ellen's code to generate a design matrix
+# Available variables are ['beta', 'progress', 'too_ill_to_move', 'contrib_foi', 'user', 'length_day', 
+# 'plength_day', 'UV', 'initial_inf', 'static_play_at_home', 'dyn_play_at_home', 'data_dist_cutoff', 
+# ''dyn_dist_cutoff', 'play_to_work', 'work_to_play', 'local_vaccination_thesh', 'global_detection_thresh', 
+# ''daily_ward_vaccination_capacity', 'neighbour_weight_threshold', 'daily_imports'],
+# ' or to set a user parameter 'user.parameter' or '.parameter'. 
+# ' To set an index use 'parameter[index]', e.g. 'beta[2]'"
 
 library(lhs)
 library(MASS)
 source("https://raw.githubusercontent.com/dougmcneall/packages-git/master/emtools.R")
 
-# Suggested ranges
 
 # helper functions for calculating the outputs we want
 get_beta <- function(rzero,IP){
   beta <- rzero/IP
   beta
 }
-
 
 get_dt <- function(IP, incu){
   b <- -(1/IP + 1/incu)
@@ -25,66 +31,50 @@ get_dt <- function(IP, incu){
   dt
 }
 
+get_IP2 <- function(IP, IP1){
+  IP2 <- IP - IP1
+  IP2
+}
+
 exptdir <- "experiments/2020-05-12-sensitivity-analysis/"
 
-n = 3
+# Number of ensemble members in the design
+n = 50
 varnames <- c('incu', 'IP', 'rzero')
 
 lhs = maximinLHS(n = n, k = length(varnames), method = "build", dup = 1, eps = 0.05,
            maxIter = 100, optimize.on = "grid", debug = FALSE)
 
-
-param_mins  <- c(4, 2, 2)
+# minima and maxima for the parameters
+param_mins  <- c(4, 2, 2.5)
 param_maxes <- c(6, 4, 4)
 
+# rescale the lhs to the parameter maxes and mins
 design <- unnormalize(
   lhs,
   un.mins = param_mins,
   un.maxes = param_maxes
 )
 
-
 colnames(design) <- varnames
-#write.matrix(design, file = 'SA_design.csv')
+write.matrix(design, file = paste0(exptdir,'lhs.csv'))
 
-#head(design)
-
+# Calculate the model inputs (and extras)
 beta <- get_beta(rzero = design[,'rzero'], IP = design[,'IP'])
 dt <- get_dt(IP = design[,'IP'], incu = design[,'incu'])
 rzero <- design[,'rzero']
 IP <- design[,'IP']
 
 IP1 <- rep(1,length(beta))
-IP2 <- IP - IP1
+IP2 <- get_IP2(IP = IP, IP1 = IP1)
 
 Progress2 = 1/design[,'incu']
 Progress3 = 1/IP1
 Progress4 = 1/IP2
-## "It is not possible to adjust the variable beta3 to equal 0.598. 
-#Available variables are ['beta', 'progress', 'too_ill_to_move', 'contrib_foi', 'user', 'length_day', 
-#'plength_day', 'UV', 'initial_inf', 'static_play_at_home', 'dyn_play_at_home', 'data_dist_cutoff', 
-#''dyn_dist_cutoff', 'play_to_work', 'work_to_play', 'local_vaccination_thesh', 'global_detection_thresh', 
-#''daily_ward_vaccination_capacity', 'neighbour_weight_threshold', 'daily_imports'],
-#' or to set a user parameter 'user.parameter' or '.parameter'. 
-#' To set an index use 'parameter[index]', e.g. 'beta[2]'"
 
 parameters3 = cbind(beta, beta, Progress2, Progress3, Progress4, rzero, dt)
-
 colnames(parameters3) <- c("beta[3]" , "beta[4]", "progress[2]", 'progress[3]', 'progress[4]', 'r0', 'dt')
 
-#parameters3  <-  data.frame("beta[3]" = round(beta,3),
-#                           "beta[4]" = round(beta,3),
-#                           "progress[2]" = round(Progress2,3),
-#                           "progress[3]"=round(Progress3,3),
-#                           "progress[4]"=round(Progress4,3),
-#                       #                       HospitalRate = hosp,
-                       #                       CriticalRate = crit,
-#                           "r0"=round(rzero,3),
-#                           "dt"=round(dt,3))
-
-#write.table(as.matrix(parameters3[,1:5]),paste0(exptdir,"ncovparams.csv"),row.names=F,col.names =F,sep=",")
-
-#write.csv(parameters3,paste0(exptdir, "ncovparams_r0dt.csv"))
 
 write.matrix(as.matrix(parameters3), paste0(exptdir,"design.csv"))
 write.matrix(as.matrix(parameters3[,1:5]), paste0(exptdir,"ncovparams.csv"))
